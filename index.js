9b2423c2b3b250f736cfa89f245afdaeead8be42
AbdulR11 <abdulrehan985@gmail.com>
2024-05-27 19:27:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+let count = 0
+let saveCount = 0
+
+let fajrEl = document.getElementById("fajr-el")
+let dhuhrEl = document.getElementById("dhuhr-el")
+let asrEl = document.getElementById("asr-el")
+let maghribEl = document.getElementById("maghrib-el")
+let ishaEl = document.getElementById("isha-el")
+
+let countEl = document.getElementById("count-el")
+
+function increment() 
+{
+    count += 1
+    countEl.textContent = count
+}
+
+function save() 
+{
+    if (saveCount == 0)
+    {
+        fajrEl.textContent += count
+        countEl.textContent = 0
+         resetCount()
+    }
+    
+      else if (saveCount == 1)
+    {
+        dhuhrEl.textContent += count
+        countEl.textContent = 0
+         resetCount()
+
+    }
+    
+      else if (saveCount == 2)
+    {
+        asrEl.textContent += count
+        countEl.textContent = 0
+         resetCount()
+
+    }
+    
+        else if (saveCount == 3)
+    {
+        maghribEl.textContent += count
+        countEl.textContent = 0
+         resetCount()
+
+    }
+    
+    
+      else if (saveCount == 4)
+    {
+        ishaEl.textContent += count
+        countEl.textContent = 0
+        resetCount()
+    }
+
+}
+
+
+function resetCount() 
+{
+    count = 0;
+    countEl.textContent = count;
+    saveCount++;
+}
+
+
+function reset() {
+    count = 0;
+    saveCount = 0;
+    countEl.textContent = count;
+    fajrEl.textContent = "Fajr: ";
+    dhuhrEl.textContent = "Dhuhr: ";
+    asrEl.textContent = "Asr: ";
+    maghribEl.textContent = "Maghrib: ";
+    ishaEl.textContent = "Isha: ";
+}
